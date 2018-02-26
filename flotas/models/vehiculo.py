0274593f2eeaf1549f6da76318227cbf4f824666
sergiosanz <sanzestradasergio@gmail.com>
2018-02-26 10:24:45 +0100

    Tercera Basurilla
    Comentando codigo para encontrar el por que no instala bien y poder puntar

--- flotas/models/vehiculo.py
+++ flotas/models/vehiculo.py
@@ -7,7 +7,7 @@ class vehiculo(models.Model):
     _name = 'flotas.vehiculo'
 
     marca = fields.Char(String="marca")
-    color = type = fields.Selection(selection=[('1', 'Blanco'), ('2', 'Gris'), ('3', 'Negro')])
+    color = fields.Selection(selection=[('1', 'Blanco'), ('2', 'Gris'), ('3', 'Negro')])
     asientos = fields.Integer(String="asientos")
     idsconductores = fields.Many2Many(String= "conductores", comodel_name='flotas.conductor', relation = 'rel_vehiculos_conductores', column1='vehiculo', column2='conductor')
     idsviajes = fields.One2many('flotas.viaje', 'idvehiculo', String="viaje")
--- flotas/models/viaje.py
+++ flotas/models/viaje.py
@@ -6,9 +6,9 @@ from datetime import timedelta,date,datetime
 class viaje(models.Model):
     _name = 'flotas.viaje'
 
-    idvehiculo = fields.Many2one('flotas.vehiculo', String="vehiculo")
-    provorigen = fields.One2many(String="origen")
-    provdestino = fields.One2many(String="destino")
+#   idvehiculo = fields.Many2one('flotas.vehiculo', String="vehiculo")
+#   provorigen = fields.One2many(String="origen")
+#   provdestino = fields.One2many(String="destino")
     fecha = fields.Date(String="fecha")
     duracionhoras = fields.Integer(String="horas")
 #   más2horas = 
